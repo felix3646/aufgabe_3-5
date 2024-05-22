447efaf9388c42ddcc3de4479144b56d4e1844f8
felix3646 <felix.sturm2004@icloud.com>
2024-05-22 12:07:14 +0200

    added functions

--- 3/read_pandas.py
+++ 3/read_pandas.py
@@ -38,30 +38,58 @@ def read_acivity_csv():
 
     # Einlesen eines Dataframes
     path = "data/activities/activity.csv"
-    df = pd.read_csv(path, sep=",", header=None)
+    df = pd.read_csv(path, sep=",")
 
     # Gibt den geladen Dataframe zurück
     return df
 
 def statistics_power(df):
+
     # Berechnung der statistischen Kennzahlen
     p_mean = df['PowerOriginal'].mean()
     p_maximum = df["PowerOriginal"].max()
 
     return p_mean, p_maximum
 
+def statistics_heartrate(df):  
 
-def make_activity_plot(df):
+    # Berechnung der statistischen Kennzahlen
+    hr_maximum = df["HeartRate"].max()
+
+    return hr_maximum
+
+def plot_power_heartrate(df):
 
     # Erstellte einen Line Plot, der ersten 2000 Werte mit der Zeit aus der x-Achse
-    fig = px.line(df.head(2000), x= "Zeit in ms", y="Messwerte in mV")
+    fig = px.line(df, y=["PowerOriginal", "HeartRate"])
+
+    # Hinzufügen von Layout-Elementen
+    fig.update_layout(
+        xaxis_title="Zeit in s",
+        yaxis_title="Power in Watt / Herzfrequenz in bpm")
+
     return fig
 
+def add_HR_Zones(df):
+
+    # Definition der Herzfrequenzzonen
+    zone_1_min = 0.5*statistics_heartrate(df)
+    zone_1_max = 0.6*statistics_heartrate(df)
+    zone_2_max = 0.7*statistics_heartrate(df)
+    zone_3_max = 0.8*statistics_heartrate(df)
+    zone_4_max = 0.9*statistics_heartrate(df)
+    zone_5_max = 1.0*statistics_heartrate(df)
+
+    # Hinzufügen der Zonen in df
+    df["Zone 1"] = (df["HeartRate"] >= zone_1_min) & (df["HeartRate"] < zone_1_max)
+    df["Zone 2"] = (df["HeartRate"] >= zone_1_max) & (df["HeartRate"] < zone_2_max)
+    df["Zone 3"] = (df["HeartRate"] >= zone_2_max) & (df["HeartRate"] < zone_3_max)
+    df["Zone 4"] = (df["HeartRate"] >= zone_3_max) & (df["HeartRate"] < zone_4_max)
+    df["Zone 5"] = (df["HeartRate"] >= zone_4_max) & (df["HeartRate"] < zone_5_max)
+
+    return df
 
 if __name__ == "__main__":
     df = read_acivity_csv()
-    print(df.head())
-
-    #meane, maximume = statistics_power(df)
-    #print(meane)
-    #print(maximume)
+    dfnew = add_HR_Zones(df)
+    print(dfnew.head())
